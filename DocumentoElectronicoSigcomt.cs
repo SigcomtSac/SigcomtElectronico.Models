1f9c0a3ea0f723066c654375cef55a68646a2ac8
camicasus <mcarbajal@sigcomt.com>
2019-02-28 09:49:57 -0500

    Ajuste en Modelo

--- DocumentoElectronicoSigcomt.cs
+++ DocumentoElectronicoSigcomt.cs
@@ -5,9 +5,6 @@ namespace SigcomtElectronico.Models
 {
     public class DocumentoElectronicoSigcomt : DocumentoElectronico
     {
-        public string OrderReference { get; set; }
-        public string CondicionPago { get; set; }
-        public string FechaVencimiento { get; set; }
         public string Match { get; set; }
 
         public new List<DetalleDocumentoElectronicoSigcomt> Items { get; set; }
